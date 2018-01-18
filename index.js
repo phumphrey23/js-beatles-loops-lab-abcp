117c5b599c5de6e3656443c34e9b6acef7665bf4
Precious Humphrey <phumphrey23@yahoo.com>
2018-01-18 15:33:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,15 +8,16 @@ function theBeatlesPlay(musicians, instruments){
 
 function johnLennonFacts(array){
   var array =[];
+  var n = 0;
   var facts = [
     "He was the last Beatle to learn to drive",
     "He was never a vegetarian",
     "He was a choir boy and boy scout",
     "He hated the sound of his own voice"
   ];
-  while(0 < facts.length) {
+  while(n < facts.length) {
     array.push(facts[] + '!!!')
+    n++
   };
-  0++;
   return array;
 };
